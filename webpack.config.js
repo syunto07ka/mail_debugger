ee28a38f9eb10a70610cb0f3c19b443759f1126f
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-06-07 17:10:00 +0900

    feat: set webpack config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HTMLPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".json"]
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: path.join(__dirname, "public/index.html")
+    })
+  ]
+};
